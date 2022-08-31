f279e05324cbf11bbedeb8ce69102b3cb1f9b08e
disection <diseccionmexico@gmail.com>
2022-08-31 01:38:47 -0500

    add css

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="bgimg-1">
+        <div className="caption">        
+          <h1>Tienda Oficial Nascar México</h1>
+            <h3>!Espérala muy pronto!</h3>
+        </div>
+      </div></>
   );
 }
 
